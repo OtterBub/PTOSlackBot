4d4d71f3884efaf7afbecc73a074db372625bc79
OtterBub <gamepsk@hanmail.net>
2022-04-10 16:20:11 +0900

    create testgspred.py
    add sheetdata.json
    - included sheet key data

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # PTOSlackBot
 
 need more file: "key.json"
+
+# how to get "key.json" file
+1. login or create project google Cloud Platform
+2. Google Drive API Enable
+3. Google Sheet API Enable
+4. Create Service Account
+- [IAM & Admin] -> [Service Accounts] -> [CREATE SERVICE ACCOUNT] Click
+- and Service Account -> [Action] -> [Manage Keys]
+- and [ADD key] -> [Create new key] -> Key type select [JSON] -> [CREATE]
+5. Done
--- main.py
+++ main.py
@@ -9,7 +9,8 @@ scope = {
 }
 
 json_key_path = "./key.json"
-json_file = open(json_key_path, 'r', encoding='utf-8')
+json_sheetdata_path = "./sheetdata.json"
+json_file = open(json_sheetdata_path, 'r', encoding='utf-8')
 json_data = json.load(json_file)
 
 # json load sheet key load
@@ -18,34 +19,3 @@ json_sheet_key = json_data['sheet_key']
 
 credential = ServiceAccountCredentials.from_json_keyfile_name(json_key_path, scope)
 gc = gspread.authorize(credential)
-
-# Function
-def test_spreadsheet(doc:gspread.Spreadsheet):
-    sheet = doc.worksheet("Sheet1")
-    test_val = sheet.get('A1')
-    print(test_val)
-
-# Use URL
-spreadsheet_url = "https://docs.google.com/spreadsheets/d/" + json_sheet_key + "/edit?usp=sharing"
-doc_url = gc.open_by_url(spreadsheet_url)
-
-print("doc_url")
-test_spreadsheet(doc_url)
-
-# OR
-
-# Use Key
-spreadsheet_key = json_sheet_key
-doc_key = gc.open_by_key(spreadsheet_key)
-
-print("doc_key")
-test_spreadsheet(doc_key)
-
-# OR
-
-# Use Name
-spreadsheet_name = "Slack Bot Test Sheet"
-doc_name = gc.open(spreadsheet_name)
-
-print("doc_name")
-test_spreadsheet(doc_name)
